e6caa1834b0bdd8c851a2996693bfdca3c6821a6
Daniel Pujazon Bonet <danielpujazonbonet@gmail.com>
2018-05-24 23:09:21 +0200

    Init DrawerCart

---
+++ DrawerCart.js
@@ -0,0 +1,16 @@
+
+//////////////////////////////////////////////////////////////////////////////////////////
+////Name:	load_drawer
+////Pre:	Must be at least two divs: The main's div with id "main"
+////		and the div's drawercart with id="drawer_cart"
+///Post: 	While loading the site, will put the drawers cart at right side of the screen 
+////		and hides it.
+//////////////////////////////////////////////////////////////////////////////////////////
+
+function load_drawer(){
+
+}
+
+function HelloWorld(){
+	alert("Hello World!");
+}
