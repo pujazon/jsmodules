4622a4b5a0c374ddbc426ac9b02723952a46027a
pujazon <danielpujazonbonet@gmail.com>
2018-06-07 23:15:27 +0200

    OnResize implementado

--- DrawerCart/DrawerCart.js
+++ DrawerCart/DrawerCart.js
@@ -9,16 +9,22 @@ function HelloWorld(){
 
 function OpenDrawerCart(){
 	var drawer_cart= document.getElementById("drawer_cart");
-	ToggleClasses(drawer_cart,"hide","display");
-	MoveToLeft(drawer_cart,300,"medium");
-}
+	if (drawer_cart.classList.contains("hide")){
+		ToggleClasses(drawer_cart,"hide","display");
+		MoveToLeft(drawer_cart,300,"medium","open");
+	}
 
+	//Control del OnResize()
+	store_lwsize();
+
+}
 
 function CloseDrawerCart(){
 	var drawer_cart= document.getElementById("drawer_cart");
-	MoveToRight(drawer_cart,300,"medium");
+	if (drawer_cart.classList.contains("display")){
+		MoveToRight(drawer_cart,300,"medium","close");
+	}
 }
-
 
 
 function ToggleClasses(elem,cborrar,cañadir){
@@ -27,17 +33,21 @@ function ToggleClasses(elem,cborrar,cañadir){
 }
 
 
-function MoveToLeft(elem,size,speed) {
+function MoveToLeft(elem,size,speed,type) {
 
- 	var pos = window.innerWidth
-	|| document.documentElement.clientWidth
-	|| document.body.clientWidth;
+	var pos;
 
-	var chunk = parse_speed(speed);
-	printf(chunk);
+	if(type=="resize_left") pos= (document.getElementById("drawer_cart").getBoundingClientRect().left);
+	else if (type=="open") pos = window.innerWidth	|| document.documentElement.clientWidth	|| document.body.clientWidth;
+
+	var chunk = parse_speed(speed,size);
 	var count = 0;
 
-	var id = setInterval(frame, 10);
+	var delay;
+	if(type=="resize_left") delay=1;
+	else if (type=="open") delay=10;
+
+	var id = setInterval(frame, delay);
 	function frame() {
 		if (count == size) {
 			clearInterval(id);
@@ -47,29 +57,81 @@ function MoveToLeft(elem,size,speed) {
 			elem.style.left = (pos-count) + 'px'; 
 		}
 	}
+	store_lwsize();	
 }
 
 
-function MoveToRight(elem,size,speed) {
 
-	var pos= (document.getElementById("drawer_cart").getBoundingClientRect().left);
+function MoveToRight(elem,size,speed,type) {
 
-	var chunk = parse_speed(speed);
+	var pos;
+
+	pos= (document.getElementById("drawer_cart").getBoundingClientRect().left);
+	
+	var chunk = parse_speed(speed,size);
 	var count = 0;
 
-	var id = setInterval(frame, 10);
+
+	var delay;
+	if(type=="resize_right") delay=1;
+	else if (type=="close") delay=10;
+
+	var id = setInterval(frame, 1);
 	function frame() {
 		if (count == size) {
-			ToggleClasses(drawer_cart,"display","hide");
+			if(type=="close"){ToggleClasses(drawer_cart,"display","hide");
+		}
 			clearInterval(id);
 		} 
 		else {
 			count += chunk;  
-			elem.style.left = (pos+count) + 'px'; 
+			var sum = (pos+count);
+			elem.style.left = (sum) + 'px'; 
 		}
 	}
+
+	//Por race condition o scope variable no se guarda el nuevo valor.
+	//Poner el teorico valor correcto para que en el segundo resize no haya error
+
+	var posf= (document.getElementById("drawer_cart").getBoundingClientRect().left);
+	store_lwsize();
 	
 }
+
+function OnResizeDrawerCart(){
+
+	var drawer_cart= document.getElementById("drawer_cart");
+	var dist;
+
+ 	var wsize = window.innerWidth
+	|| document.documentElement.clientWidth
+	|| document.body.clientWidth;
+
+	var last_wsize = document.getElementById("wsize").innerHTML;
+
+	//Si es un resize a pantalla mas grande, hide.
+	//Pero a la vez recolocarlo para futuros open
+
+	if(last_wsize < wsize){
+		dist = wsize-last_wsize;
+		MoveToRight(drawer_cart,dist,"inst","resize_right");
+	}
+	//Else: Haremos el Move hazia esa dirección.
+	else{
+		dist = last_wsize-wsize;
+		MoveToLeft(drawer_cart,dist,"inst","resize_left");		
+	}
+
+}
+
+function store_lwsize(){
+	document.getElementById("wsize").innerHTML=window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+}
+
+function load_lwsize(){
+	printf("load_lwsize() == "+document.getElementById("wsize").innerHTML);
+}
+
 
 function printscreen(){
 	var w = window.innerWidth
@@ -86,9 +148,11 @@ function printscreen(){
 
 function printf(string){console.log(string);}
 
-function parse_speed(speed){
+function parse_speed(speed,size){
 
 	var chunk;
+
+	printf("parse() = size == "+size);
 
 	switch (speed) {
 	    case "low":
@@ -100,6 +164,9 @@ function parse_speed(speed){
 	    case "fast":
 	        chunk = 40;
 	        break;
+	    case "inst":
+	        chunk = size;
+	        break;	        
 	}
 
 	return chunk;
